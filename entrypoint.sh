b151fde3d7c47388cf99fe43dc5894b5f0f4a3c5
M Rifki Septiawan <rifkim91@gmail.com>
2025-08-18 16:01:17 +0700

    fix: new pgsql config

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,16 +2,9 @@
 
 cd /var/www/html
 
-# Run one-time setup tasks
-php artisan session:table
-
-php artisan migrate:fresh --force
-
-# Set proper permissions for the storage directory
 chmod -R 775 /var/www/html/storage
 chown -R www-data:www-data /var/www/html/storage
 
-# Run cache commands for production performance
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
