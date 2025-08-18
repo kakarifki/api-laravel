2d90943bc06aa60d95de138d29cfcd6e628540a1
M Rifki Septiawan <rifkim91@gmail.com>
2025-08-18 15:10:07 +0700

    fix: add string length on email migration

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
         });
 
         Schema::create('password_reset_tokens', function (Blueprint $table) {
-            $table->string('email')->primary();
+            $table->string('email', 100)->primary(); // Tambahkan panjang string eksplisit
             $table->string('token');
             $table->timestamp('created_at')->nullable();
         });
