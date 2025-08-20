80239fc0548c1229c83b1e9a624e26cb8fb27cae
M Rifki Septiawan <rifkim91@gmail.com>
2025-08-20 21:56:01 +0700

    feat: simplize entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,11 +2,14 @@
 
 cd /var/www/html
 
-chmod -R 775 /var/www/html/storage
-chown -R www-data:www-data /var/www/html/storage
-
+# Bersihkan cache Laravel
 php artisan config:cache
 php artisan route:cache
 php artisan view:cache
 
+# Atur izin direktori
+chmod -R 775 /var/www/html/storage
+chown -R www-data:www-data /var/www/html/storage
+
+# Mulai server
 exec apache2-foreground
